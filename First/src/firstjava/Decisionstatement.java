8f6b8fc5577f42b8a1de8ce3b5eabab64c84c0cd
sohaibsahil <sohaibahmad5598@gmail.com>
2022-10-08 22:51:08 +0500

    First Commit

---
+++ First/src/firstjava/Decisionstatement.java
@@ -0,0 +1,20 @@
+package firstjava;
+
+public class Decisionstatement {
+
+	public static void main(String[] args) {
+		
+		int x=10;
+		int y=20;
+		if(x>y)
+		{
+			System.out.println("X values are greater than y");
+			
+		}
+		else {
+			System.out.println("Value of Y is greater");
+			
+		}
+	}
+	
+}
