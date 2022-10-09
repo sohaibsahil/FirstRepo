38ded1afdeac31cdf144721d51b3fc97c387682e
sohaibsahil <sohaibahmad5598@gmail.com>
2022-10-09 16:20:34 +0500

    For loop done

---
+++ First/src/firstjava/Forloop.java
@@ -0,0 +1,16 @@
+package firstjava;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<=10; i++)// For loop to show number from 1 - 10
+		{
+			int sum=i+1;
+			System.out.println(sum);
+			
+		}
+	}
+	
+	
+}
